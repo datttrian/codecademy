a0a7890d1d23a8e52eee3d92cda47fdc6ff6a050
dattranvnu <dattranvnu@gmail.com>
2023-02-12 23:14:49 -0800

    Add URL pattern for viewing memories

--- places_remember/memories/urls.py
+++ places_remember/memories/urls.py
@@ -2,11 +2,13 @@
 The urlpatterns route URL to specific pages in the web app.
 """
 from django.urls import path
+from django.views.decorators.cache import never_cache
 from . import views
 
 app_name = 'memories'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('memory-creation', views.AddMemoryView.as_view(), name='memory_creation')
+    path('memory-creation', views.AddMemoryView.as_view(), name='memory_creation'),
+    path('home', never_cache(views.UserMemoriesView.as_view()), name='home'),
 ]
