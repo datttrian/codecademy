8b0c456bb2c571d92405534fd48e0192748fc84c
dattranvnu <dattranvnu@gmail.com>
2023-02-12 18:06:45 -0800

    Add widget_tweaks to installed apps

--- places_remember/places_remember/settings.py
+++ places_remember/places_remember/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'memories',
     'social_django',
+    'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
