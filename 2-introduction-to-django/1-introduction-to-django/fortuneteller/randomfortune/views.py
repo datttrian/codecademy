244364c3d7dc9ea7b8b2dca55568ccc0dca9043c
dattranvnu <dattranvnu@gmail.com>
2023-02-14 19:12:57 -0800

    Add 'Project: Fortune Teller'

---
+++ 2-introduction-to-django/1-introduction-to-django/fortuneteller/randomfortune/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+import random
+# Create your views here.
+
+fortuneList = [
+    "All will go well with your new project.",
+   "If you continually give, you will continually have.",
+   "Self-knowledge is a life long process.",
+   "You are busy, but you are happy.",
+   "Your abilities are unparalleled.",
+   "Those who care will make the effort.",
+   "Now is the time to try something new.",
+   "Miles are covered one step at a time.",
+   "Don’t just think, act!"
+]
+
+def fortune(request):
+    fortune = random.choice(fortuneList)
+    context = {"fortune": fortune}
+    return render(request, "randomfortune/fortune.html", context)
--- 2-introduction-to-django/README.md
+++ 2-introduction-to-django/README.md
@@ -1,3 +1,7 @@
 # Introduction to Django Track
 
 Using your knowledge of Python, explore the world of web development!
+
+# Projects:
+
+* [Fortune Teller](1-introduction-to-django/fortuneteller)
