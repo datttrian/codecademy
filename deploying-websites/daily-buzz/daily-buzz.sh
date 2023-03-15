94c8ae8fc318b1b2e37281feeef0b7c856385d8f
dattranvnu <dattranvnu@gmail.com>
2023-03-15 04:27:45 -0700

    Add 'Project: Daily Buzz'

---
+++ deploying-websites/daily-buzz/daily-buzz.sh
@@ -0,0 +1,27 @@
+$ pwd
+/home/ccuser/workspace/daily-buzz
+$ ls
+culture  health  national  technology
+$ cd national/politics
+$ ls
+policy
+$ mkdir elections
+$ cd elections
+$ touch candidates.txt
+$ touch issues.txt
+$ cd ../../../
+$ pwd
+/home/ccuser/workspace/daily-buzz
+$ mkdir business
+$ cd business
+$ ls
+$ mkdir startups
+$ cd startups
+$ cd ../
+$ mkdir startups/disruptors
+$ touch startups/disruptors/tech.txt
+$ touch startups/disruptors/design.txt
+$ touch startups/disruptors/education.txt
+$ cd ..
+$ pwd
+/home/ccuser/workspace/daily-buzz
